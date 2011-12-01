f866e5bef517171b3175cd72509607b54dd87ed1
Daniel Barnes <dabarnes2b@gmail.com>
2011-12-01 13:32:25 -0800

    people can now join and what everyone else has done already

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
+  , th = require('thoonk').createClient('localhost')
+
+var sortedfeed = th.sortedFeed('didyoudraw');
 
 app.listen(8000);
 
@@ -19,12 +22,24 @@ function handler (req, res) {
 
 var conns = {};
 io.sockets.on('connection', function (socket) {
+	items = sortedfeed.getAll(function(err, reply) {
+		if (reply.length > 0) {
+			Object.keys(reply).forEach(function(key) {
+ 				socket.send(reply[key].item);
+			});
+		}
+	});
 	conns[socket.id] = socket;
 	socket.on('disconnect', function(){
 		delete conns[socket.id];
 	});
 	socket.on('message', function(msg) {
-		socket.send(msg);
-		socket.broadcast.emit('message', msg);
+		sortedfeed.append(msg, msg, function(item , id){
+			socket.broadcast.emit('message',msg);
+			socket.send(msg);
+		});
+	});
+	socket.on('clear', function(tmp){
+		socket.broadcast.emit('clear',1);
 	});
 });
