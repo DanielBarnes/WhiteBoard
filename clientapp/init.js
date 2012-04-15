11b1da00515442cdf94f7d1abee6fb7e6e479cde
Daniel Barnes <dabarnes2b@gmail.com>
2012-04-15 06:54:36 +0000

    client on data case setup

--- clientapp/init.js
+++ clientapp/init.js
@@ -4,6 +4,19 @@ console.log('socket connected to ' + document.URL);
 var clientBoard = require('./clientBoard'),
     roomBoard = require('./roomBoard'),
     mainBoard = require('./mainBoard'),
-//    EventEmitter = require('./EventEmitter');
+    EventEmitter = require('./EventEmitter');
 
-
+socket.on('data',function(data){
+    switch(data.type){
+        case 'checkin' :
+            console.log('checkin received');
+            break;
+        case 'shape':
+            roomBoard.draw(data);
+            break;
+        case 'clear':
+            roomBoard.clear();
+            mainBoard.clear();
+            break;
+    }
+});
