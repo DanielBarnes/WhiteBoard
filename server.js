729be752b5721cdea1d1637f8d838ec562e5a1e5
Daniel Barnes <dabarnes2b@gmail.com>
2012-04-18 20:15:05 +0000

    commit and push tobe up to date

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ whiteServer.emit('go', app);
 app.get('/whiteboard/:id', function(req,res){
     console.log("added room: " + req.params.id);
     whiteServer.emit('room' ,req.params.id);
+    res.render('whiteboard.jade', {layout: true, title: 'derp'});
 });
 //stitch route for whiteboard.js
 var package = stitch.createPackage({
